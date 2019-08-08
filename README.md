46d980a6d8746307cf24c953deea0cb97c08ee65
stigma0617 <sun008sun@naver.com>
2019-08-08 11:31:28 +0900

    init_VOV-FCOS

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# VoVNet-FCOS
-FCOS with VoVNet backbone networks
+# Impelmentation of [FCOS](https://arxiv.org/abs/1904.01355) with [VoVNet](https://arxiv.org/abs/1904.09730)(CVPRW'19) Efficient Backbone Networks
+
+This repository contains [FCOS](https://github.com/facebookresearch/maskrcnn-benchmark)(ICCV'19) with [VoVNet](https://arxiv.org/abs/1904.09730) (CVPRW'19) efficient backbone networks. This code based on pytorch imeplementation of [FCOS](https://github.com/tianzhi0549/FCOS) 
+
+## Highlights
+
+- Memory efficient 
+- Better performance, especially for *small* object
+- Faster speed
+
+
+
+## Comparison with ResNet backbones
+
+- same hyperparameters
+- same training protocols ( max epoch, learning rate schedule, etc)
+- 8 x TITAN Xp GPU
+- pytorch1.1
+- CUDA v9
+- cuDNN v7.2
+
+
+| Backbone  | Multi-scale training | Inference time (ms) | Box AP (AP/APs/APm/APl)  | DOWNLOAD |
+|----------|:---------------:|:-------------------:|:------------------------:| :---:|
+| R-50-FPN-1x       | No           | 84                  | 37.5/21.3/40.3/49.5      | - |
+ **V-39**-FPN-1x       | No           |**82**                 | 37.7/**22.4**/41.8/48.4      | [link](https://dl.dropbox.com/s/8n0wyypfggliplw/FCOS-V-39-FPN-1x.pth?dl=1)|
+ ||
+| R-101-FPN-2x       | Yes           | 104                  | 41.3/25.0/45.5/53.0      | -                          
+| **V-57**-FPN-2x        |Yes           | **91**                  | 41.6/**25.9**/45.6/53.1      |  [link](https://dl.dropbox.com/s/f1posfwebb2ynnp/FCOS-V-57-MS-FPN-2x.pth?dl=1)|
+||
+| R-101-32x8d-FPN-2x        | Yes           |171                   | 42.5/26.0/46.1/54.2      | -  |
+| **V-93**-FPN-2x        | Yes           | **113**                  |   42.1/**26.2**/46.0/53.9    | [link](https://dl.dropbox.com/s/2v7go4lenvvjd1s/FCOS-V-93-MS-FPN-2x.pth?dl=1)|
+
+
+
+## ImageNet pretrained weight
+
+- [VoVNet-39](https://dl.dropbox.com/s/s7f4vyfybyc9qpr/vovnet39_statedict_norm.pth?dl=1)
+- [VoVNet-57](https://dl.dropbox.com/s/b826phjle6kbamu/vovnet57_statedict_norm.pth?dl=1)
+- [VoVNet-75](https://dl.dropbox.com/s/ve1h1ol2ge7yfta/vovnet75_statedict_norm.pth.tar?dl=1)
+- [VoVNet-93](https://dl.dropbox.com/s/qtly316zv1isn0t/vovnet93_statedict_norm.pth.tar?dl=1)
+
+
+## Installation
+
+Check [INSTALL.md](INSTALL.md) for installation instructions which is orginate from [FCOS](https://github.com/tianzhi0549/FCOS#installation)
+
+
+
+## Training
+Follow [the instructions](https://github.com/tianzhi0549/FCOS#training)
+
+For example,
+
+```bash
+# specify the number of GPU you can use.
+export NGPUS=8 
+python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/vovnet/fcos_V_39_FPN_1x.yaml" 
+```
+
+## Evaluation
+Follow [the instructions](https://github.com/tianzhi0549/FCOS#inference)
+First of all, you have to download the weight file you want to inference.
+For examaple,
+##### multi-gpu evaluation & test batch size 16,
+
+```bash
+wget https://www.dropbox.com/s/8n0wyypfggliplw/FCOS-V-39-FPN-1x.pth?dl=0
+export NGPUS=8
+python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/test_net.py --config-file "configs/vovnet/fcos_V_39_FPN_1x.yaml" TEST.IMS_PER_BATCH 16 MODEL.WEIGHT FCOS-V-39-FPN-1x.pth
+```
+
+##### single-gpu evaluation & test batch size 1,
+
+```bash
+wget https://www.dropbox.com/s/8n0wyypfggliplw/FCOS-V-39-FPN-1x.pth?dl=0
+CUDA_VISIBLE_DEVICES=0
+python tools/test_net.py --config-file "configs/vovnet/e2e_faster_rcnn_V_39_FPN_2x.yaml" TEST.IMS_PER_BATCH 1 MODEL.WEIGHT FCOS-V-39-FPN-1x.pth
+```
+
+## Related projects
+
+[VoVNet-Classification](https://github.com/vov-net/VoVNet-Detectron)
+[VoVNet-Detectron](https://github.com/vov-net/VoVNet-Classification)
---
+++ fcos_core/modeling/backbone/vovnet.py
@@ -0,0 +1,246 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from collections import OrderedDict
+from maskrcnn_benchmark.utils.registry import Registry
+from maskrcnn_benchmark.layers import FrozenBatchNorm2d
+from maskrcnn_benchmark.modeling.make_layers import group_norm
+
+_GN = False
+
+VoVNet27FPNStagesTo5 = {
+    'config_stage_ch': [64, 80, 96, 112],
+    'config_concat_ch': [128, 256, 384, 512],
+    'layer_per_block': 5,
+    'block_per_stage': [1, 1, 1, 1]
+}
+
+VoVNet39FPNStagesTo5 = {
+    'config_stage_ch': [128, 160, 192, 224],
+    'config_concat_ch': [256, 512, 768, 1024],
+    'layer_per_block': 5,
+    'block_per_stage': [1, 1, 2, 2]
+}
+
+VoVNet57FPNStagesTo5 = {
+    'config_stage_ch': [128, 160, 192, 224],
+    'config_concat_ch': [256, 512, 768, 1024],
+    'layer_per_block': 5,
+    'block_per_stage': [1, 1, 4, 3]
+}
+
+VoVNet69FPNStagesTo5 = {
+    'config_stage_ch': [128, 160, 192, 224],
+    'config_concat_ch': [256, 512, 768, 1024],
+    'layer_per_block': 5,
+    'block_per_stage': [1, 1, 6, 3]
+}
+
+VoVNet93FPNStagesTo5 = {
+    'config_stage_ch': [128, 160, 192, 224],
+    'config_concat_ch': [256, 512, 768, 1024],
+    'layer_per_block': 5,
+    'block_per_stage': [1, 3, 8, 3]
+}
+
+_STAGE_SPECS = Registry({
+    "V-27-FPN": VoVNet27FPNStagesTo5,
+    "V-39-deFPN": VoVNet39FPNStagesTo5,
+    "V-39-FPN": VoVNet39FPNStagesTo5,
+    "V-57-FPN": VoVNet57FPNStagesTo5,
+    "V-69-FPN": VoVNet69FPNStagesTo5,
+    "V-93-FPN": VoVNet93FPNStagesTo5,
+    "V-39-FPN-RETINANET": VoVNet39FPNStagesTo5,
+    "V-57-FPN-RETINANET": VoVNet57FPNStagesTo5,
+    "V-93-FPN-RETINANET": VoVNet93FPNStagesTo5
+})
+
+def freeze_bn_params(m):
+    """Freeze all the weights by setting requires_grad to False
+    """
+    m.eval()
+    for p in m.parameters():
+        p.requires_grad = False
+
+def conv3x3(in_channels, out_channels, module_name, postfix, stride=1, groups=1, kernel_size=3, padding=1):
+    """3x3 convolution with padding"""
+    return [
+        (f'{module_name}_{postfix}/conv',
+         nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=False)),
+        (f'{module_name}_{postfix}/norm',
+            group_norm(out_channels) if _GN else FrozenBatchNorm2d(out_channels)
+        ),
+        (f'{module_name}_{postfix}/relu', nn.ReLU(inplace=True))
+    ]
+
+
+def conv1x1(in_channels, out_channels, module_name, postfix, stride=1, groups=1, kernel_size=1, padding=0):
+    """3x3 convolution with padding"""
+    return [
+        (f'{module_name}_{postfix}/conv',
+         nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups,
+                   bias=False)),
+        (f'{module_name}_{postfix}/norm',
+            group_norm(out_channels) if _GN else FrozenBatchNorm2d(out_channels)
+        ),
+        (f'{module_name}_{postfix}/relu', nn.ReLU(inplace=True))
+    ]
+
+
+class _OSA_module(nn.Module):
+
+    def __init__(self, in_ch, stage_ch, concat_ch, layer_per_block, module_name, identity=False):
+        super(_OSA_module, self).__init__()
+
+        self.identity = identity
+        self.layers = nn.ModuleList()
+        in_channel = in_ch
+        for i in range(layer_per_block):
+            self.layers.append(nn.Sequential(OrderedDict(conv3x3(in_channel, stage_ch, module_name, i))))
+            in_channel = stage_ch
+
+        # feature aggregation
+        in_channel = in_ch + layer_per_block * stage_ch
+        self.concat = nn.Sequential(OrderedDict(conv1x1(in_channel, concat_ch, module_name, 'concat')))
+
+    def forward(self, x):
+
+        identity_feat = x
+
+        output = []
+        output.append(x)
+        for layer in self.layers:
+            x = layer(x)
+            output.append(x)
+
+        x = torch.cat(output, dim=1)
+        xt = self.concat(x)
+
+        if self.identity:
+            xt = xt + identity_feat
+
+        return xt
+
+
+class _OSA_stage(nn.Sequential):
+
+    def __init__(self, in_ch, stage_ch, concat_ch, block_per_stage, layer_per_block, stage_num):
+        super(_OSA_stage, self).__init__()
+
+        if not stage_num == 2:
+            self.add_module('Pooling', nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True))
+
+        module_name = f'OSA{stage_num}_1'
+        self.add_module(module_name, _OSA_module(in_ch, stage_ch, concat_ch, layer_per_block, module_name))
+
+        for i in range(block_per_stage - 1):
+            module_name = f'OSA{stage_num}_{i + 2}'
+            self.add_module(module_name,
+                            _OSA_module(concat_ch, stage_ch, concat_ch, layer_per_block, module_name, identity=True))
+
+
+
+class VoVNet(nn.Module):
+
+    def __init__(self, cfg):
+
+        super(VoVNet, self).__init__()
+
+        global _GN
+        _GN = cfg.MODEL.VOVNET.USE_GN
+        stage_specs = _STAGE_SPECS[cfg.MODEL.BACKBONE.CONV_BODY]
+
+        config_stage_ch = stage_specs['config_stage_ch']
+        config_concat_ch = stage_specs['config_concat_ch']
+        block_per_stage = stage_specs['block_per_stage']
+        layer_per_block = stage_specs['layer_per_block']
+
+
+
+        # self.stem = nn.Sequential()
+        # Stem module
+        stem = conv3x3(3, 64, 'stem', '1', 2)
+        stem += conv3x3(64, 64, 'stem', '2', 1)
+        stem += conv3x3(64, 128, 'stem', '3', 2)
+        self.add_module('stem', nn.Sequential((OrderedDict(stem))))
+
+        stem_out_ch = [128]
+        in_ch_list = stem_out_ch + config_concat_ch[:-1]
+        # OSA stages
+        self.stage_names = []
+        for i in range(4):  # num_stages
+            name = 'stage%d' % (i + 2)
+            self.stage_names.append(name)
+            self.add_module(name, _OSA_stage(in_ch_list[i],
+                                             config_stage_ch[i],
+                                             config_concat_ch[i],
+                                             block_per_stage[i],
+                                             layer_per_block,
+                                             i + 2))
+
+        # initialize weights
+        self._initialize_weights()
+        # Optionally freeze (requires_grad=False) parts of the backbone
+        self._freeze_backbone(cfg.MODEL.BACKBONE.FREEZE_CONV_BODY_AT)
+
+    # def _initialize_weights(self, cfg):
+    #     if cfg.MODEL.WEIGHT is not None:
+    #         if not os.path.isfile(cfg.MODEL.WEIGHT):
+    #             raise RuntimeError(f'=> {cfg.MODEL.WEIGHT} not found')
+    #         checkpoint = torch.load(cfg.MODEL.WEIGHT)
+    #         pretrain_dict = checkpoint['state_dict']
+    #         model_dict = {}
+    #         state_dict = self.state_dict()
+    #         for k, v in pretrain_dict.items():
+    #             if k.startswith('module.classifier'):
+    #                 continue
+    #             k = k.replace('module.features.', '')
+    #             if 'stem' in k:
+    #                 k = 'stem.' + k
+    #             if k in state_dict:
+    #                 model_dict[k] = v
+    #                 # print(f'{k} loaded')
+    #         state_dict.update(model_dict)
+    #         self.load_state_dict(state_dict)
+    #     else:
+    #         for m in self.modules():
+    #             if isinstance(m, nn.Conv2d):
+    #                 nn.init.kaiming_normal_(m.weight)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+
+    def _freeze_backbone(self, freeze_at):
+        if freeze_at < 0:
+            return
+        # freeze BN layers
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                freeze_bn_params(m)
+        for stage_index in range(freeze_at):
+            if stage_index == 0:
+                m = self.stem # stage 0 is the stem
+            else:
+                m = getattr(self, "stage" + str(stage_index+1))
+            for p in m.parameters():
+                p.requires_grad = False
+
+    def forward(self, x):
+        x = self.stem(x)
+        outputs = []
+        for name in self.stage_names:
+            x = getattr(self, name)(x)
+            outputs.append(x)
+
+        return outputs
+
+    # def train(self, mode=True):
+    #     super().train(mode)
+    #     for m in self.modules():
+    #         if isinstance(m, nn.BatchNorm2d):
+    #             freeze_bn_params(m)
+
